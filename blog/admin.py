e9f1f925676e33adc69d0eaa3fcdd7895d550fe8
tuhanovos <saitpage2011@yandex.ru>
2018-09-13 00:20:43 +0300

    News commit 12.09.2018

---
+++ blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import UserCreateNews
+
+# Register your models here.
+
+
+admin.site.register(UserCreateNews)
---
+++ blog/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, re_path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from blog.views import user_add_news, view_one_post
+
+urlpatterns = [
+    path('profile', user_add_news, name='profile'),
+    re_path(r'^post/(?P<news_id>\d+)/', view_one_post, name='post'),
+    re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns() + static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
